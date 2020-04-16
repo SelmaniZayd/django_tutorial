cfe6f78793aa4e2d0d5e8da57b4f052aeac53bb5
SelmaniZayd <selmani.zayd@gmail.com>
2020-04-16 11:50:35 +0200

    testing the index view

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,6 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
-from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
 
@@ -18,9 +17,7 @@ class IndexView(generic.ListView):
         Return the last five published questions (not including those set to be
         published in the future).
         """
-        return Question.objects.filter(
-            pub_date__lte=timezone.now()
-        ).order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     model = Question
