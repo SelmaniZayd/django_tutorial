c5ca5aaaedb1f3a80e970d081e613caf808bbe36
SelmaniZayd <selmani.zayd@gmail.com>
2020-04-17 09:59:04 +0200

    customizing admin page

--- polls/models.py
+++ polls/models.py
@@ -14,6 +14,9 @@ class Question(models.Model):
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
@@ -21,5 +24,5 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
 
     def __str__(self):
-        return self.question_text
+        return self.choice_text
     
